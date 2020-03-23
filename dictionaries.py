e19714adb0fc875415f3df937d50e54888a0abdc
Shekhar Kumar <zatch216@gmail.com>
2020-03-23 20:46:31 +0530

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,34 @@
+#creating and printing sample dictionary
+sample = {"#1key":"#1value","#2key":"#2value","#3key":"#3value","#4key":"#4value"}
+print("Printing the sample dictionary: {}".format(sample))
+
+#entering data into a dictionary
+sample["#5key"] = "#5value"
+print("sample after inputing new entry: {}".format(sample))
+#printing particular value by using key
+print(sample["#2key"])
+
+#deleting an entry
+del sample["#5key"]
+print(sample)
+
+while True:
+    key = input("Enter what you want to search in sample")
+    if key == "quit":
+        break
+    if key in sample:
+        #print(sample[key])
+        val = sample.get(key)
+        print(val)
+    else :
+        print(key +" doesn't exist")
+
+while True:
+    key =input("Enter the key you want to add\n")
+    if key == "quit":
+        break
+    val =input("enter coresponding value of the key\n")
+    if key != "quit":
+        sample[key]=val
+
+print(sample)
