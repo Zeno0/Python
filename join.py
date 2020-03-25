7b641fbceb1dcbfa871e1ddf87d0e88cd774d163
Shekhar Kumar <zatch216@gmail.com>
2020-03-25 12:57:48 +0530

    Update join.py

--- join.py
+++ join.py
@@ -26,12 +26,13 @@ exits = [{"Q":0},
          {"N":1,"W":2,"Q":0},
           {"W":2,"S":1,"Q":0}]
 
-print(exits[1])
 
-# LOCATIONS AND EXITS ARE SREATED USING 'sample' pic
+# LOCATIONS AND EXITS ARE CREATED USING 'sample' pic
 
 loc = 1 # default location is 1 i.e at the road
-while True:
+
+while False: # to run this loop just type "True" in while loop
+   
     ava_exit = ", " .join(exits[loc].keys())
     print(locations[loc])
 
@@ -41,8 +42,46 @@ while True:
     dir = input("Available exits are: "+ava_exit+" ").upper()  # .upper() is to convert input un uppercase
     print()
     if dir in exits[loc]:
-        loc = exits[loc][dir]
-        print(loc)
+        loc = exits[loc][dir] # update the location
     else:
         print("You  can't go there ")
+# this variable is used to create 'exits_' as a dictionary
+exits_ = {0:{"Q":0},
+          1:{"W":2,"E":3,"N":5,"S":4,"Q":0},
+          2:{"N":5,"Q":0},
+          3:{"W":1,"Q":0},
+          4:{"N":1,"W":2,"Q":0},
+          5:{"W":2,"S":1,"Q":0}}
+vocab = {"QUIT" : "Q",
+         "NORTH" : "N",
+         "SOUTH" : "S",
+         "EAST" : "E",
+         "WEST" : "W"} 
 
+while True:
+    ava_exit = ", " .join(exits_[loc].keys())
+    print(locations[loc])
+
+    if loc == 0:  #to exit from while loop and prints the value at 0 key of locations
+        break
+
+    dir = input("Available exits are: "+ava_exit+" ").upper()  # .upper() is to convert input un uppercase
+    print()
+    if len(dir)>1:  # it will check if len of input is greater the 1 or not
+        words = dir.split()  # it is helpful when there is 100s of dictionary
+        for word in words:
+            if word in vocab:
+                dir = vocab[word]
+                break
+    if dir in exits_[loc]:
+        loc = exits_[loc][dir] # update the location
+    else:
+        print("You  can't go there ")
+print('-' * 50)
+# showing how to use split() function
+print(locations[0].split())
+print(locations[3].split(","))
+print(' '.join(locations[0].split()))
+
+for i in locations[0].split():
+    print(i)
