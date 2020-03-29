36a72d312b4b36f115e846bc0e101472c57ff25d
Shekhar Kumar <zenobell143@gmail.com>
2020-03-29 21:59:41 +0530

    Create turtledemo.py

---
+++ turtledemo.py
@@ -0,0 +1,18 @@
+# two ways of importing
+
+# import turtle
+
+# turtle.forward(150)
+# turtle.right(250)
+# turtle.forward(150)
+# turtle.circle(106)
+# turtle.done()
+
+from turtle import forward, right, circle, done
+
+forward(150)
+right(250)
+forward(150)
+circle(106)
+done()
+
