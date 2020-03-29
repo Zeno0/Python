ff1f4814d138cdc48475640d979fae0bc0ac9c06
Shekhar Kumar <zenobell143@gmail.com>
2020-03-29 21:58:59 +0530

    Create shelvesample_3.py

---
+++ shelvesample_3.py
@@ -0,0 +1,49 @@
+import shelve
+book = shelve.open("book_db")
+book["anime"] = {"DBZ" : ["Goku","vegeta","gohan"],
+                 "oregairu" : ["hachiman","yukino","yui"],
+                 "naruto" : ["naruto","sasuke","sakura"],
+                 "eureka7" : ["eruka","holland","renton"]}
+book ["music"] = {"Green day" : ["American idiot", "21 Guns", "Extraordinary Girl"],
+                  "Linkin park" : ["One more light", "iridescent", "In the end"]}
+
+print(book["anime"])
+print(book["music"])
+print(book["anime"]["oregairu"])
+book.close()
+print('-'*50)
+
+# modifing the previous dictionary_challenge to use shelve
+locations = shelve.open('location')
+vocabulary = shelve.open('vocabulary')
+
+loc = '1'
+while True:
+    availableExits = ", ".join(locations[loc]["exits"].keys())
+ 
+    print(locations[loc]["desc"])
+ 
+    if loc == '0':
+        break
+    else:
+        allExits = locations[loc]['exits'].copy()
+        allExits.update(locations[loc]["namedExits"])
+ 
+    direction = input("Available exits are " + availableExits).upper()
+    print()
+ 
+    # Parse the user input, using our vocabulary dictionary if necessary
+    if len(direction) > 1:  # more than 1 letter, so check vocab
+        words = direction.split()
+        for word in words:
+            if word in vocabulary:   # does it contain a word we know?
+                direction = vocabulary[word]
+                break
+ 
+    if direction in allExits:
+        loc = allExits[direction]
+    else:
+        print("You cannot go in that direction")
+
+locations.close()
+vocabulary.close()
